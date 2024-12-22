44c6a81773e655f16bf9332bd206a1881a9f73da
Daniil Rusanov <daniil12099@gmail.com>
2024-12-22 19:00:05 +0200

    Daniil Rusanov created the skeleton of the contact_us page and the footer component.

---
+++ src/main/java/com/historycode/ui/component/footer/FooterComponent.java
@@ -0,0 +1,11 @@
+package com.historycode.ui.component.footer;
+
+import com.historycode.ui.component.BaseComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FooterComponent extends BaseComponent {
+    public FooterComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
---
+++ src/main/java/com/historycode/ui/elements/contact_us/ContactUsContentElement.java
@@ -0,0 +1,10 @@
+package com.historycode.ui.elements.contact_us;
+
+import com.historycode.ui.elements.BaseElement;
+import org.openqa.selenium.WebDriver;
+
+public class ContactUsContentElement extends BaseElement {
+    public ContactUsContentElement(WebDriver driver) {
+        super(driver);
+    }
+}
