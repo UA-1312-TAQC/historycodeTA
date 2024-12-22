c291c54c06f7efb9c382e6e96cd283b4b8d676a1
Daryna Boruk <darynaboruk@gmail.com>
2024-12-23 00:55:24 +0200

    skeleton for the teamPage of adminpanel was created

---
+++ src/main/java/com/historycode/ui/page/adminpanel/teamPage/TeamManagementElement.java
@@ -0,0 +1,10 @@
+package com.historycode.ui.page.adminpanel.teamPage;
+
+import com.historycode.ui.elements.BaseElement;
+import org.openqa.selenium.WebDriver;
+
+public class TeamManagementElement extends BaseElement {
+    public TeamManagementElement(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/main/java/com/historycode/ui/page/adminpanel/teamPage/TeamMemberElement.java
@@ -0,0 +1,10 @@
+package com.historycode.ui.page.adminpanel.teamPage;
+
+import com.historycode.ui.elements.BaseElement;
+import org.openqa.selenium.WebDriver;
+
+public class TeamMemberElement extends BaseElement {
+    public TeamMemberElement(WebDriver driver) {
+        super(driver);
+    }
+}
