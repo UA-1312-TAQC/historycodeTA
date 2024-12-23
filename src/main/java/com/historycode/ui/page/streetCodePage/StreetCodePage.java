26c2a7b27e30d36138209059cefc4c83f28b30af
nestea4 <panchoshnaanastasia@gmail.com>
2024-12-23 13:17:10 +0300

    added modals

--- src/main/java/com/historycode/ui/page/streetCodePage/StreetCodePage.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/StreetCodePage.java
@@ -44,45 +44,54 @@ public class StreetCodePage extends BasePage {
 
     @FindBy(xpath = "//div[@class='donateBtnContainer']")
     private WebElement quickDonateButtonNode;
+    @FindBy(xpath = "//div[@class='ant-modal-content']")
+    private WebElement surveyModalNode;
 
     private BreadcrumbsElement breadcrumbs;
     private ScrollTopButtonElement scrollTopButton;
     private QuickDonateButtonElement quickDonateButton;
-    private StreetCodeMainCardComponent mainCard;
+    private MainCardComponent mainCard;
     private StreetCodeDetailsComponent details;
     private FactCardComponent facts;
     private TimelineComponent timeline;
     private RelatedFiguresComponent relatedFigures;
     private AdditionalInfoComponent additionalInfo;
-    private PartnersCarouselComponent partners;
+    private PartnerComponent partners;
     private RunningLineComponent runningLine;
     private VerticalProgressComponent verticalProgress;
+    private SurveyModal surveyModal;
+
     public StreetCodePage(WebDriver driver){
         super(driver);
         this.breadcrumbs = new BreadcrumbsElement(driver);
         this.scrollTopButton = new ScrollTopButtonElement(driver);
         this.quickDonateButton = new QuickDonateButtonElement(driver);
-        this.mainCard = new StreetCodeMainCardComponent(driver, mainCardNode);
+        this.mainCard = new MainCardComponent(driver, mainCardNode);
         this.details = new StreetCodeDetailsComponent(driver, detailsNode);
         this.facts = new FactCardComponent(driver, factsNode);
         this.timeline = new TimelineComponent(driver, timelineNode);
         this.relatedFigures = new RelatedFiguresComponent(driver, relatedFiguresNode);
         this.additionalInfo = new AdditionalInfoComponent(driver, additionalInfoNode);
-        this.partners = new PartnersCarouselComponent(driver, partnersNode);
+        this.partners = new PartnerComponent(driver, partnersNode);
         this.runningLine = new RunningLineComponent(driver, runningLineNode);
         this.verticalProgress = new VerticalProgressComponent(driver, verticalProgressNode);
+        this.surveyModal = new SurveyModal(driver, surveyModalNode);
     }
 }
 
+//elements
 class QuickDonateButtonElement extends BaseElement {
     public QuickDonateButtonElement(WebDriver driver) {
         super(driver);
     }
 }
 
-class StreetCodeMainCardComponent extends BaseComponent {
-    public StreetCodeMainCardComponent(WebDriver driver, WebElement rootElement) {
+//components
+class MainCardComponent extends BaseComponent {
+    private KeywordPersonsModal keywordPersonsModal;
+    public MainCardComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        this.keywordPersonsModal = new KeywordPersonsModal(driver, rootElement);
     }
 }
 
@@ -93,8 +102,10 @@ class StreetCodeDetailsComponent extends BaseComponent {
 }
 
 class FactCardComponent extends BaseComponent {
+    private FactCardModal factCardModal;
     public FactCardComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        this.factCardModal = new FactCardModal(driver, rootElement);
     }
 }
 
@@ -111,13 +122,15 @@ class RelatedFiguresComponent extends BaseComponent {
 }
 
 class AdditionalInfoComponent extends BaseComponent {
+    private AdditionalInfoModal additionalInfoModal;
     public AdditionalInfoComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        this.additionalInfoModal = new AdditionalInfoModal(driver, rootElement);
     }
 }
 
-class PartnersCarouselComponent extends BaseComponent {
-    public PartnersCarouselComponent(WebDriver driver, WebElement rootElement) {
+class PartnerComponent extends BaseComponent {
+    public PartnerComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
 }
@@ -130,6 +143,31 @@ class RunningLineComponent extends BaseComponent {
 
 class VerticalProgressComponent extends BaseComponent {
     public VerticalProgressComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
+
+//modals
+class KeywordPersonsModal extends BaseModal {
+    public KeywordPersonsModal(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
+
+class FactCardModal extends BaseModal {
+    public FactCardModal(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
+
+class AdditionalInfoModal extends BaseModal {
+    public AdditionalInfoModal(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
+
+class SurveyModal extends BaseModal {
+    public SurveyModal(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
 }
