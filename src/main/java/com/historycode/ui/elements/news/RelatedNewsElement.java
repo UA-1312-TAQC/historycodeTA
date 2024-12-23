580236f9e8c59ddeba6cd0c60d6cf5a723a8c888
NastiaDamian <nastuhta2005@gmail.com>
2024-12-23 11:29:14 +0300

    Add news-page

---
+++ src/main/java/com/historycode/ui/elements/news/RelatedNewsElement.java
@@ -0,0 +1,10 @@
+package com.historycode.ui.elements.news;
+
+import com.historycode.ui.elements.BaseElement;
+import org.openqa.selenium.WebDriver;
+
+public class RelatedNewsElement extends BaseElement {
+    public RelatedNewsElement(WebDriver driver) {
+        super(driver);
+    }
+}
