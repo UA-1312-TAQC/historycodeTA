f12f2abdfeb38665a90b830772d3fe491922408c
yevhen.katruk <fdblackwood@gmail.com>
2024-12-25 20:02:17 +0200

    refactor homePage

--- src/main/java/com/historycode/ui/page/BasePage.java
+++ src/main/java/com/historycode/ui/page/BasePage.java
@@ -1,11 +1,13 @@
 package com.historycode.ui.page;
 
 import com.historycode.ui.Base;
+import com.historycode.ui.component.footer.FooterComponent;
 import com.historycode.ui.component.header.HeaderComponent;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 
 @Getter
@@ -14,13 +16,18 @@ public abstract class BasePage extends Base {
     @FindBy(xpath = "//div[@class='HeaderBlock']")
     private WebElement headerNode;
 
+    @FindBy(css = ".footerContainer")
+    private WebElement footerNode;
 
 
     protected HeaderComponent header;
+    protected FooterComponent footer;
 
     public BasePage(WebDriver driver) {
         super(driver);
-        this.header = new HeaderComponent(driver, this.headerNode);
+        PageFactory.initElements(driver, this);
+        this.header = new HeaderComponent(driver, headerNode);
+        this.footer = new FooterComponent(driver, footerNode);
 
     }
 }
---
+++ src/main/java/com/historycode/ui/page/homePage/PartnersComponent.java
@@ -0,0 +1,11 @@
+package com.historycode.ui.page.homePage;
+
+import com.historycode.ui.component.BaseComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+class PartnersComponent extends BaseComponent {
+    public PartnersComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+}
