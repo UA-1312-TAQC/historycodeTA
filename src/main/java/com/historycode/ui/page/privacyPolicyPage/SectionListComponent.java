e11ee655413e3ce2971f0dff5f317eb4c15f65f7
Vova K. <101831233+Vovanchek@users.noreply.github.com>
2024-12-27 13:25:47 +0200

    Add files via upload

---
+++ src/main/java/com/historycode/ui/page/privacyPolicyPage/SectionListComponent.java
@@ -0,0 +1,62 @@
+package com.historycode.ui.page.privacyPolicyPage;
+
+import com.historycode.ui.component.BaseComponent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionListComponent extends BaseComponent {
+
+        @FindBy(xpath = ".//section")
+        private List<WebElement> sectionComponent;
+
+        @FindBy(xpath = ".//div[@class='content']")
+        private List<WebElement> contentComponent;
+
+        public SectionListComponent(WebDriver driver, WebElement rootElement) {
+            super(driver, rootElement);
+
+        }
+
+        public List<String> SectionsName() {
+            List<String> ListOfSections = new ArrayList<>();
+            for(WebElement element : sectionComponent) {
+                WebElement titleElement = element.findElement(By.xpath(".//div[@class='title']"));
+                ListOfSections.add(titleElement.getText());
+            }
+            return ListOfSections;
+        }
+
+        public List<String> SectionsText() {
+            List<String> ListOfSectionsText = new ArrayList<>();
+            for(WebElement element : sectionComponent) {
+                WebElement contentElement = element.findElement(By.xpath(".//div[@class='content']"));
+                ListOfSectionsText.add(contentElement.getText());
+            }
+            return ListOfSectionsText;
+        }
+
+        public List<String> SectionsLinks() {
+            List<String> ListOfSectionsLinks = new ArrayList<>();
+            for(WebElement element : contentComponent) {
+                try {
+                    List<WebElement> links = element.findElements(By.xpath(".//a[@class='link']"));
+
+                    if (!links.isEmpty()) {
+                        for (WebElement link : links) {
+                            ListOfSectionsLinks.add(link.getText());
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error processing element: " + e.getMessage());
+                }
+            }
+
+            return ListOfSectionsLinks;
+        }
+
+}
---
+++ src/main/java/com/historycode/ui/page/privacyPolicyPage/TitleContainerComponent.java
@@ -0,0 +1,37 @@
+package com.historycode.ui.page.privacyPolicyPage;
+
+import com.historycode.ui.component.BaseComponent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TitleContainerComponent extends BaseComponent {
+
+    @FindBy(xpath = ".//div[@class='title']")
+    private WebElement titleContainer;
+
+    public TitleContainerComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public String titleName() {
+        String name;
+        WebElement nameElement = titleContainer.findElement(By.xpath(".//div[@class='titleBig']"));
+        return nameElement.getText();
+    }
+
+    public String subTitle() {
+        String subTitleText;
+        WebElement subTitleElement = titleContainer.findElement(By.xpath(".//div[@class='subTitle']"));
+        return subTitleElement.getText();
+    }
+    public String disclaimer() {
+        String disclaimerText;
+        WebElement disclaimerElement = titleContainer.findElement(By.xpath("//div[@class='disclaimer']"));
+        return disclaimerElement.getText();
+    }
+
+
+
+}
