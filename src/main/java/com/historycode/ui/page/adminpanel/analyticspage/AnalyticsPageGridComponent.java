be31b71b22d2e2315d8a0b896485a8589eae100b
Viktor Kravchenko <viktorkrav03@gmail.com>
2024-12-30 11:03:37 +0200

    analytics page improvement

---
+++ src/main/java/com/historycode/ui/page/adminpanel/analyticspage/AnalyticsPageGridComponent.java
@@ -0,0 +1,22 @@
+package com.historycode.ui.page.adminpanel.analyticspage;
+
+import com.historycode.ui.component.adminPanel.gridAdminPanel.BaseGridComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class AnalyticsPageGridComponent extends BaseGridComponent {
+
+
+
+    //TODO add element search
+    List<AnalyticsRowCompoment> analyticsRowCompoments;
+    public AnalyticsPageGridComponent(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public AnalyticsRowCompoment getRowById(int id){ return analyticsRowCompoments.get(id);}
+
+    public int getRowCount(){ return analyticsRowCompoments.size();}
+}
