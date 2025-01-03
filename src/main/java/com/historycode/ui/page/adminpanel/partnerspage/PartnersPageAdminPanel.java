d73a7a238533a302fe6cbf9c2ec9a126149f0f41
Ivan <ivanromanuk2014@gmail.com>
2025-01-03 11:47:13 +0200

    Add methods to PartnersPage

--- src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageAdminPanel.java
+++ src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageAdminPanel.java
@@ -1,17 +1,29 @@
 package com.historycode.ui.page.adminpanel.partnerspage;
 
 import com.historycode.ui.page.adminpanel.BasePageAdminPanel;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class PartnersPageAdminPanel extends BasePageAdminPanel {
-    
-    @FindBy(xpath = "")
+
+    @FindBy(xpath = "//button[span[text()='Створити партнера']]")
     WebElement addNewPartnersButton;
-    PartnersPageGridComponent PartnersPageGridComponent;
+    @FindBy(xpath = "//div[@class='partners-page']//div[@class='partners-page-container']")
+    WebElement rooElement;
+    PartnersPageGridComponent partnersPageGridComponent;
 
     public PartnersPageAdminPanel(WebDriver driver) {
         super(driver);
+        partnersPageGridComponent = new PartnersPageGridComponent(driver, rooElement);
+    }
+
+    public void clickAddNewPartnersButton() {
+        addNewPartnersButton.click();
+    }
+
+    public PartnersPageGridComponent getPartnersPageGridComponent() {
+        return partnersPageGridComponent;
     }
 }
--- src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageGridComponent.java
+++ src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageGridComponent.java
@@ -6,16 +6,40 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.historycode.ui.component.adminPanel.gridAdminPanel.BaseGridComponent;
-import com.historycode.ui.component.adminPanel.paginationAdminPanel.PaginationAdminPanelComponent;
 
 import lombok.Getter;
 
 @Getter
 public class PartnersPageGridComponent extends BaseGridComponent {
 
-    List<PartnersRowComponent> partnersRowComponents;
+    protected List<PartnersRowComponent> partnersRowComponents;
 
     public PartnersPageGridComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
+
+    public List<PartnersRowComponent> getPartnersRowComponents() {
+        return partnersRowComponents;
+    }
+
+    public PartnersRowComponent getRowById(int id) {
+        return partnersRowComponents.get(id);
+    }
+
+    public int getRowCount() {
+        return partnersRowComponents.size();
+    }
+
+    public void clickNextPage() {
+        pagination.clickNextPage();
+    }
+
+    public void clickPrevPage() {
+        pagination.clickPrevPage();
+    }
+
+    public void clickPaginationItem(int index) {
+        pagination.clickPaginationItem(index);
+    }
+
 }
