d55080e379cdd7140c39a357809a2bbc2f77229b
Viktor Kravchenko <viktorkrav03@gmail.com>
2025-01-07 12:02:44 +0200

    Created login page for admin panel

---
+++ src/main/java/com/historycode/ui/page/adminpanel/loginpage/LoginPageAdminPanel.java
@@ -0,0 +1,50 @@
+package com.historycode.ui.page.adminpanel.loginpage;
+
+import com.historycode.ui.page.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPageAdminPanel extends BasePage {
+
+    private final String LOGIN_CSS = "form .ant-form-item:first-child div";
+    private final String PASSWORD_CSS = "form .ant-form-item:nth-child(2) div";
+
+
+    private LoginInputComponent login;
+    private LoginInputComponent password;
+
+
+    @FindBy(css = "button.loginBtn")
+    private WebElement sighInButton;
+
+
+    public LoginPageAdminPanel(WebDriver driver) {
+        super(driver);
+        login = new LoginInputComponent(driver, driver.findElement(By.cssSelector(LOGIN_CSS)));
+        password = new LoginInputComponent(driver, driver.findElement(By.cssSelector(PASSWORD_CSS)));
+    }
+
+    public String getLoginLabel(){return login.getLabel();}
+    public String getPasswordLabel(){return password.getLabel();}
+
+    public void enterLogin(String inputData){login.fillInput(inputData);}
+
+    public void enterPassword(String inputData){password.fillInput(inputData);}
+
+
+    //TODO set correct page as a return value
+    /*
+    public StreetCodeCatalogPageAdminPanel clickSighInButtonPositive(){
+        sighInButton.click();
+        return new StreetCodeCatalogPageAdminPanel
+    }
+*/
+
+    public void clickSighInButtonNegative(){
+        sighInButton.click();
+    }
+
+}
+
