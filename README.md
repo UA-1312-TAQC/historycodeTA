9dba0f6638945c36bb035447dd7a0f56e3aeca01
Liubomyr Halamaha <galamagal86@gmail.com>
2025-01-07 14:35:25 +0200

    added base config for allure

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ create file `src/test/resources/config.properties`
 base.ui.url=base_frontend_url
 implicitlyWait=10
 ```
+
+
+run allure serve
+```shell
+allure serve .\target\allure-results\
+```
--- src/test/java/com/historycode/ui/SmokeTest.java
+++ src/test/java/com/historycode/ui/SmokeTest.java
@@ -3,15 +3,18 @@ package com.historycode.ui;
 
 import com.historycode.ui.page.homePage.HomePage;
 import com.historycode.ui.testrunners.BaseTestRunner;
+import io.qameta.allure.Issue;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SmokeTest extends BaseTestRunner {
 
+    @Issue("91")
     @Test
     public void testOpenPage(){
         HomePage homePage = new HomePage(driver);
+        homePage.getHeader().scrollToEndOfPage();
 
         Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div[1]/div[1]/div[1]")).isDisplayed());
 
