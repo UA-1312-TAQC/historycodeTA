5c98a9e93c831038e549da47e49067af2509facd
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-09 15:31:48 +0200

    change StreetCodesPage - KH

--- src/main/java/com/historycode/ui/page/streetcodes/StreetCodesPage.java
+++ src/main/java/com/historycode/ui/page/streetcodes/StreetCodesPage.java
@@ -1,16 +1,14 @@
 package com.historycode.ui.page.streetcodes;
 import com.historycode.ui.page.BasePage;
-import com.historycode.ui.page.streetCodePage.StreetCodePage;
-
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
-@Getter
+
+
 public class StreetCodesPage extends BasePage {
 
     @FindBy(xpath = "//h1[@class='streetcodeCatalogHeading']")
@@ -19,14 +17,16 @@ public class StreetCodesPage extends BasePage {
     @FindBy(xpath = "//p[@class='streetcodeCatalogCaption']")
     private WebElement streetsCodesCaptionNode;
 
-    @FindBy(xpath = "//div[@class='steetcodeCatalogContainer']")
-    private WebElement containerRootNode;
-
-    private final CatalogComponent streetCodesCatalogComponent;
+    @Getter
+    private CatalogComponent streetCodesCatalogComponent;
+    @Getter
+    private CatalogItemComponent catalogItemComponent;
 
     public StreetCodesPage(WebDriver driver) {
         super(driver);
-        streetCodesCatalogComponent = new CatalogComponent(driver, containerRootNode);
+        PageFactory.initElements(driver, this);
+        this.streetCodesCatalogComponent = new CatalogComponent(driver);
+        this.catalogItemComponent = new CatalogItemComponent(driver);
     }
 
     public String getStreetCodesTitle() {
@@ -37,50 +37,11 @@ public class StreetCodesPage extends BasePage {
         return streetsCodesCaptionNode.getText();
     }
 
-    public List<String> getCatalogNames() {
-        return streetCodesCatalogComponent.getItemComponents()
-                .stream()
-                .map(CatalogItemComponent::getName)
-                .collect(Collectors.toList());
-    }
 
-    public List<String> getCatalogDescriptions() {
-        return streetCodesCatalogComponent.getItemComponents()
-                .stream()
-                .map(CatalogItemComponent::getDescription)
-                .collect(Collectors.toList());
-    }
+    public void clickOnCatalogItem(int index) {
+        CatalogComponent catalogComponent = getStreetCodesCatalogComponent();
+            catalogComponent.clickCatalogElement(index);
 
-    private void validateIndex(int index, List<?> list) {
-        if (index < 0 || index >= list.size()) {
-            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
-        }
-    }
-
-    public String getCatalogItemName(int index) {
-        List<String> catalogNames = getCatalogNames();
-        validateIndex(index, catalogNames);
-
-        return catalogNames.get(index);
-    }
-
-    public String getCatalogItemDescription(int index) {
-        List<String> catalogDescriptions = getCatalogDescriptions();
-        validateIndex(index, catalogDescriptions);
-
-        return catalogDescriptions.get(index);
-    }
-
-    public StreetCodePage clickCatalogItem(int index) {
-
-        List<CatalogItemComponent> items = streetCodesCatalogComponent.getItemComponents();
-        validateIndex(index, items);
-
-        items
-            .get(index)
-            .getRootNode()
-            .click();
-
-        return new StreetCodePage(driver);
     }
 }
+
