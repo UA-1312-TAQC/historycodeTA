8c86c056d7084c4292468ec65a361012cd9ba425
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-09 15:28:04 +0200

    add burger manu - KH

--- src/main/java/com/historycode/ui/page/BasePage.java
+++ src/main/java/com/historycode/ui/page/BasePage.java
@@ -1,6 +1,7 @@
 package com.historycode.ui.page;
 
 import com.historycode.ui.Base;
+import com.historycode.ui.component.BurgerMenu.BurgerMenuComponent;
 import com.historycode.ui.component.footer.FooterComponent;
 import com.historycode.ui.component.header.HeaderComponent;
 import lombok.Getter;
@@ -18,12 +19,28 @@ public abstract class BasePage extends Base {
     @FindBy(xpath = "//div[@class='footerContainer']")
     private WebElement footerNode;
 
+    @FindBy(xpath = "//div[contains(@class, 'rightPartContainer')]//div[contains(@class, 'drawerContainer')]//div")
+    private WebElement burgerMenu;
+
     protected HeaderComponent header;
     protected FooterComponent footer;
+    protected BurgerMenuComponent burgerMenuComponent;
 
     public BasePage(WebDriver driver) {
         super(driver);
         this.header = new HeaderComponent(driver, this.headerNode);
         this.footer = new FooterComponent(driver, this.footerNode);
+        this.burgerMenuComponent = new BurgerMenuComponent(driver, this.burgerMenu);
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean isBurgerMenuVisible() {
+        sleep(5000);
+        return burgerMenu.isDisplayed();
+    }
+
+    public void openBurgerMenu() {
+        sleep(5000);
+        burgerMenu.click();
     }
 }
