202e904a9e0d432907c7100751a46775c2e01939
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-09 15:29:34 +0200

    change CatalogComponent - KH

--- src/main/java/com/historycode/ui/page/streetcodes/CatalogComponent.java
+++ src/main/java/com/historycode/ui/page/streetcodes/CatalogComponent.java
@@ -2,51 +2,38 @@ package com.historycode.ui.page.streetcodes;
 
 import com.historycode.ui.component.BaseComponent;
 import lombok.Getter;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import org.openqa.selenium.support.PageFactory;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Getter
-class CatalogComponent extends BaseComponent {
-    private final String CATALOG_ITEMS_LOCATOR = ".//a[@class='catalogItem']";
 
-    @FindBy(xpath = CATALOG_ITEMS_LOCATOR)
+public class CatalogComponent extends BaseComponent {
+
+    @Getter
+    @FindBy(xpath = ".//a[@class='catalogItem']")
     private List<WebElement> catalogElements;
-    private final List<CatalogItemComponent> itemComponents;
 
-    CatalogComponent(WebDriver driver, WebElement rootElement) {
-        super(driver, rootElement);
-        itemComponents = catalogElements.stream()
-                .map(element -> new CatalogItemComponent(driver, element))
-                .collect(Collectors.toList());
+    public CatalogComponent(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    private void updateItemsAfterScroll() {
-        List<WebElement> updatedElements = rootElement.findElements(By.xpath(CATALOG_ITEMS_LOCATOR));
-
-        for (WebElement element: updatedElements) {
-            if (!catalogElements.contains(element)) {
-                catalogElements.add(element);
-                itemComponents.add(new CatalogItemComponent(driver, element));
-            }
+    public WebElement getCatalogElement(int index) {
+        if (index >= 0 && index < catalogElements.size()) {
+            return catalogElements.get(index);
         }
+        throw new IndexOutOfBoundsException("Invalid index: " + index);
     }
 
-    public void scrollAndWaitForNewItems() {
-        int initialSize = catalogElements.size();
-
-        scrollToEndOfPage();
-
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.numberOfElementsToBeMoreThan(By.xpath(CATALOG_ITEMS_LOCATOR), initialSize));
-
-        updateItemsAfterScroll();
+    public boolean isCatalogElementVisible(int index) {
+        return getCatalogElement(index).isDisplayed();
     }
+
+    public void clickCatalogElement(int index) {
+        getCatalogElement(index).click();
+    }
+
 }
