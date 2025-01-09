429cdb260e4dce8a473a4ce9b7045547c43b91ed
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-09 15:28:26 +0200

    Burger menu - KH

---
+++ src/main/java/com/historycode/ui/component/BurgerMenu/BurgerMenuComponent.java
@@ -0,0 +1,52 @@
+package com.historycode.ui.component.BurgerMenu;
+
+import com.historycode.ui.Base;
+import com.historycode.ui.component.BaseComponent;
+import com.historycode.ui.elements.HelpUs.CurrencyModalElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
+
+import java.time.Duration;
+import java.util.List;
+
+
+public class BurgerMenuComponent extends BaseComponent {
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(xpath = "//div[contains(@class, 'drawerContainer')]")
+    private WebElement containerMenu;
+
+    @FindBy(xpath = "//a[contains(@class, 'headerItem')]")
+    private List<WebElement> menuItems;
+
+    public BurgerMenuComponent(WebDriver driver, WebElement rootElement) {
+        super(driver);
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(new DefaultElementLocatorFactory(rootElement), this);
+    }
+
+    public void clickMenuItem(String itemName) {
+        WebElement menuItem = wait.until(ExpectedConditions.visibilityOfAllElements(menuItems))
+                .stream()
+                .filter(item -> item.getText().equalsIgnoreCase(itemName))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("Menu item " + itemName + " not found"));
+        wait.until(ExpectedConditions.elementToBeClickable(menuItem));
+        menuItem.click();
+    }
+}
+
+
+
+
+
