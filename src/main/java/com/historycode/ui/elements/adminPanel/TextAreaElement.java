59ba868966f918cbebff1d207cd37e81f5a2a64a
ivanromanuk2014 <ivanromanuk2014@gmail.com>
2025-01-11 11:20:50 +0200

    Create base class TextAreaElement

---
+++ src/main/java/com/historycode/ui/elements/adminPanel/TextAreaElement.java
@@ -0,0 +1,46 @@
+package com.historycode.ui.elements.adminPanel;
+
+
+import com.historycode.ui.component.BaseComponent;
+import lombok.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class TextAreaElement extends BaseComponent {
+
+    @Getter
+    @FindBy(xpath = "./div[contains(@class, 'ant-form-item-label')]/label")
+    private WebElement label;
+
+    @Getter
+    @FindBy(xpath = "./textarea")
+    private WebElement textAreaField;
+
+    @Getter
+    @FindBy(xpath = "./span[@class='ant-input-suffix']/span")
+    private WebElement charCounter;
+
+    public TextAreaElement(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public void setTextArea(String value) {
+        waitForTextAreaReady();
+        textAreaField.clear();
+        textAreaField.sendKeys(value);
+    }
+
+    public String getTextAreaValue() {
+        return textAreaField.getDomAttribute("value");
+    }
+
+    public String getCharCount() {
+        return charCounter.getText();
+    }
+
+    public void waitForTextAreaReady() {
+        wait.until(ExpectedConditions.visibilityOf(textAreaField));
+    }
+}
