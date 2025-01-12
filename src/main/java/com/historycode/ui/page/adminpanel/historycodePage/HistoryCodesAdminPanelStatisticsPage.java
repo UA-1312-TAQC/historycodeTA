b6ce133a368326143c9051647b1d626bdbb977fd
Daniil Rusanov <daniil12099@gmail.com>
2025-01-12 15:05:10 +0200

    Added xPaths

--- src/main/java/com/historycode/ui/page/adminpanel/historycodePage/HistoryCodesAdminPanelStatisticsPage.java
+++ src/main/java/com/historycode/ui/page/adminpanel/historycodePage/HistoryCodesAdminPanelStatisticsPage.java
@@ -1,4 +1,10 @@
 package com.historycode.ui.page.adminpanel.historycodePage;
 
-public class HistoryCodesAdminPanelStatisticsPage {
+import com.historycode.ui.page.adminpanel.BasePageAdminPanel;
+import org.openqa.selenium.WebDriver;
+
+public class HistoryCodesAdminPanelStatisticsPage extends BasePageAdminPanel {
+    public HistoryCodesAdminPanelStatisticsPage(WebDriver driver) {
+        super(driver);
+    }
 }
--- src/main/java/com/historycode/ui/page/adminpanel/historycodePage/component/HistoryCodesAdminPanelRowComponent.java
+++ src/main/java/com/historycode/ui/page/adminpanel/historycodePage/component/HistoryCodesAdminPanelRowComponent.java
@@ -2,13 +2,31 @@ package com.historycode.ui.page.adminpanel.historycodePage.component;
 
 import com.historycode.ui.component.BaseComponent;
 import com.historycode.ui.component.adminPanel.dropDownAdminPanel.DropdownComponent;
+import com.historycode.ui.page.adminpanel.historycodePage.HistoryCodesAdminPanelStatisticsPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class HistoryCodesAdminPanelRowComponent extends BaseComponent {
-    private DropdownComponent dropdown;
+
+    @FindBy(xpath = "./td[@class='ant-table-cell']")
+    private WebElement name;
+    @FindBy(xpath = "./td[@class='ant-table-cell']")
+    private WebElement id;
+    private DropdownComponent dropDown;
+    @FindBy(xpath = "./button[@class='ant-btn css-k7429z ant-btn-default ant-dropdown-trigger']")
+    private WebElement dropDownNode;
+    @FindBy(xpath = "./td[@class='ant-table-cell']")
+    private WebElement Data;
+    @FindBy(xpath = "./span[@class='anticon anticon-edit actionButton']")
+    private WebElement editPageButton;
+    @FindBy(xpath = "./span[@class='anticon anticon-delete actionButton']")
+    private WebElement deleteButton;
+    @FindBy(xpath = "./span[@class='anticon anticon-bar-chart actionButton']")
+    private WebElement statisticsPageButton;
 
     public HistoryCodesAdminPanelRowComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        this.dropDown = new DropdownComponent(driver, dropDownNode);
     }
 }
