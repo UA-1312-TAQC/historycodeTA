d8c1fb1e49df3375e33962ce532c13f528dbf604
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-13 13:21:55 +0200

    Changed and commented the methods to do with ChronologyYearsBarComponent

--- src/main/java/com/historycode/ui/page/streetCodePage/components/ChronologyYearsBarComponent.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/components/ChronologyYearsBarComponent.java
@@ -1,42 +1,94 @@
 package com.historycode.ui.page.streetCodePage.components;
 
 import com.historycode.ui.component.BaseComponent;
+import lombok.Getter;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
+@Getter
 public class ChronologyYearsBarComponent extends BaseComponent {
-    @FindBy(xpath = ".//div[@class='tickContainer']//span")
-    private List<WebElement> yearNodes;
 
-    @FindBy(xpath = ".//div[@class='tickContainer active']//span")
-    private WebElement selectedYearNode;
+//    @FindBy(xpath = ".//div[@class='tickContainer']//span")
+//    private List<WebElement> yearNodes;
+//
+//    @FindBy(xpath = ".//div[@class='tickContainer active']//span")
+//    private WebElement selectedYearNode;
+
+    @Getter
+    @FindBy(xpath = "//div[contains(@class, 'timelineYearTick')]")
+    private List<WebElement> selectedYearBoxContainer;
+
+    @Getter
+    @FindBy(xpath = "//div[contains(@class, 'active')]")
+    private WebElement activeYearBox;
 
     public ChronologyYearsBarComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        PageFactory.initElements(driver, this);
     }
 
-    public void selectYear(String year) {
-        yearNodes.stream()
-                .filter(node -> node.getText().equals(year))
-                .findFirst()
-                .ifPresent(WebElement::click);
+    public ChronologyYearsBarComponent(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
     }
 
-    public String getSelectedYear() {
-        return selectedYearNode.getText();
+    public WebElement getIdYearBox(int index) {
+        if (index >= 0 && index < selectedYearBoxContainer.size()) {
+            return selectedYearBoxContainer.get(index);
+        }
+        throw new IndexOutOfBoundsException("Invalid index: " + index);
     }
 
-    public List<String> getAllYears() {
-        return yearNodes.stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
+    public boolean isYearBoxVisible(int index) {
+        WebElement yearBox = getIdYearBox(index);
+        return yearBox.isDisplayed();
     }
 
-    public boolean isYearSelected(String year) {
-        return year.equals(getSelectedYear());
+    public boolean isYearBoxLarger(int index) {
+        WebElement selectedBox = getIdYearBox(index);
+        Dimension selectedBoxSize = selectedBox.getSize();
+
+        return selectedYearBoxContainer.stream()
+                .allMatch(box -> {
+                    Dimension otherBoxSize = box.getSize();
+                    return selectedBoxSize.getHeight() > otherBoxSize.getHeight() &&
+                            selectedBoxSize.getWidth() > otherBoxSize.getWidth();
+                });
+    }
+
+    public boolean isYearBoxActive(int index) {
+        if (index >= 0 && index < selectedYearBoxContainer.size()) {
+            WebElement yearBox = selectedYearBoxContainer.get(index);
+            return yearBox.getAttribute("class").contains("active");
+        }
+        throw new IndexOutOfBoundsException("Invalid index: " + index);
+    }
+
+    public boolean isActiveYearBoxVisible() {
+        sleep(2000);
+        return activeYearBox != null && activeYearBox.isDisplayed();
     }
 }
+
+
+//    public void selectYear(String year) {
+//        yearNodes.stream()
+//                .filter(node -> node.getText().equals(year))
+//                .findFirst()
+//                .ifPresent(WebElement::click);
+//    }
+//
+//    public List<String> getAllYears() {
+//        return yearNodes.stream()
+//                .map(WebElement::getText)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public boolean isYearSelected(String year) {
+//        return year.equals(getSelectedYear());
+//    }
