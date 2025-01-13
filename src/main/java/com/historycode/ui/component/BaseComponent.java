e384032f31171d60c3424af305a16c8e169d4ba3
Liubomyr Halamaha <galamagal86@gmail.com>
2025-01-13 13:15:33 +0200

    Added a constructor for WebDriver

--- src/main/java/com/historycode/ui/component/BaseComponent.java
+++ src/main/java/com/historycode/ui/component/BaseComponent.java
@@ -5,14 +5,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public abstract class BaseComponent extends Base {
 
     protected WebElement rootElement;
+    protected WebDriver driver;
 
     public BaseComponent(WebDriver driver, WebElement rootElement) {
         super(driver);
         this.rootElement = rootElement;
         PageFactory.initElements(new DefaultElementLocatorFactory(rootElement), this);
+    }
+
+    public BaseComponent(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(new DefaultElementLocatorFactory(driver),this);
+    }
+
+    public void scrollToElement(WebElement element) {
+        sleep(2000);
+        waitUntilElementVisible(element);
+        threadJs.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
+    }
+
+    public void scrollToEndOfPage() {
+        threadJs.executeScript("window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });");
+        sleep(1000);
+    }
+
+    public void waitUntilElementVisible(WebElement element) {
+        sleep(2000);
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
