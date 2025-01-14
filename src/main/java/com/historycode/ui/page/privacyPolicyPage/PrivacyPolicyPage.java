d7fd92c8f61b0117dcc99b57c49563a04191835c
Liubomyr Halamaha <galamagal86@gmail.com>
2025-01-14 22:16:47 +0200

    Update PrivacyPolicyPage.java

--- src/main/java/com/historycode/ui/page/privacyPolicyPage/PrivacyPolicyPage.java
+++ src/main/java/com/historycode/ui/page/privacyPolicyPage/PrivacyPolicyPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
 public class PrivacyPolicyPage extends BasePage {
 
     @FindBy(xpath = "//main")
