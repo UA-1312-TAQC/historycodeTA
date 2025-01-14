b98b5f7ebcc0058a3dfba7d704d4a2780d9ef266
Nik Niker <niker234567@gmail.com>
2025-01-14 13:45:36 +0200

    /

--- src/main/java/com/historycode/ui/page/adminpanel/editorpage/components/modals/ContextsModalComponent.java
+++ src/main/java/com/historycode/ui/page/adminpanel/editorpage/components/modals/ContextsModalComponent.java
@@ -1,14 +1,36 @@
 package com.historycode.ui.page.adminpanel.editorpage.components.modals;
 
+import com.historycode.ui.component.adminPanel.modalAdminPanel.BaseEditModal;
+import com.historycode.ui.elements.adminPanel.InputElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class ContextsModalComponent extends ModalComponent {
+public class ContextsModalComponent extends BaseEditModal {
+
+    @FindBy(xpath = "//label[@for = 'context']/../..")
+    private WebElement contextContainer;
+
+    private InputElement contextInput;
+
     public ContextsModalComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
+        this.contextInput = new InputElement(driver, contextContainer);
     }
 
-    public void enterContext(String name) {
-        inputComponent.setInput(name);
+    public ContextsModalComponent inputNewContext(String name) {
+        contextInput.setInputField(name);
+        return this;
+    }
+
+    public ContextsModalComponent saveNewContext() throws InterruptedException {
+        clickSaveButton();
+        Thread.sleep(4000);
+        return this;
+    }
+
+    public void closeModal() {
+        clickCloseButton();
     }
 }
+
---
+++ src/test/java/com/historycode/ui/adminPanel/DeleteContextEditor.java
@@ -0,0 +1,34 @@
+package com.historycode.ui.adminPanel;
+
+import com.historycode.ui.page.adminpanel.editorpage.ContextsPage;
+import com.historycode.ui.page.adminpanel.editorpage.components.rows.ContextsRowComponent;
+import com.historycode.ui.testrunners.TestRunnerWithAdmin;
+
+import io.qameta.allure.Issue;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class DeleteContextEditor extends TestRunnerWithAdmin {
+
+    @BeforeMethod
+    public void setupForDeleteContext() throws InterruptedException {
+        login();
+        driver.get(testValueProvider.getBaseUIUrl() + "admin-panel/editor");
+        ContextsPage contextsPage = new ContextsPage(driver);
+        contextsPage.addContext()
+                .inputNewContext("Context1")
+                .saveNewContext()
+                .closeModal();
+    }
+
+    @Test
+    @Issue("105")
+    public void testDeleteContext() {
+        ContextsPage contextsPage = new ContextsPage(driver);
+        ContextsRowComponent contextToDelete = contextsPage.getTableRowByTitle("Context1");
+        contextsPage.deleteTableRow(contextToDelete).clickOkButton();
+        assertNull(contextsPage.getTableRowByTitle("Context1"), "Context was not deleted successfully.");
+    }
+}
