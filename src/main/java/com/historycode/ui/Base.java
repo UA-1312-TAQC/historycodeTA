ce148841febf743611ac45404101e39c2e6dae62
Liubomyr Halamaha <galamagal86@gmail.com>
2025-01-14 17:01:02 +0200

    Update Base.java

--- src/main/java/com/historycode/ui/Base.java
+++ src/main/java/com/historycode/ui/Base.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+
 public abstract class Base {
     protected WebDriver driver;
     protected WebDriverWait wait;
