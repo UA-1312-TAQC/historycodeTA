0e0474df6c725c6f0ad51abd137557a99e5c65b0
Nik Niker <niker234567@gmail.com>
2025-01-15 13:10:01 +0200

    DeleteContext fix

--- src/test/java/com/historycode/ui/adminPanel/DeleteContextEditor.java
+++ src/test/java/com/historycode/ui/adminPanel/DeleteContextEditor.java
@@ -6,6 +6,7 @@ import com.historycode.ui.testrunners.TestRunnerWithAdmin;
 
 import io.qameta.allure.Issue;
 
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
@@ -33,8 +34,20 @@ public class DeleteContextEditor extends TestRunnerWithAdmin {
     public void testDeleteContext() {
         ContextsPage contextsPage = new ContextsPage(driver);
         ContextsRowComponent contextToDelete = contextsPage.getTableRowByTitle(contextName);
+        assertNotNull(contextToDelete, "Context should exist before deletion: " + contextName);
         contextsPage.deleteTableRow(contextToDelete).clickOkButton();
-        assertNull(contextsPage.getTableRowByTitle(contextName), "Context was not deleted successfully.");
+        assertNull(contextsPage.getTableRowByTitle(contextName),
+                String.format("Context '%s' still exists after deletion", contextName));
+    }
+
+    @AfterMethod
+    public void cleanup() {
+    // Clean up any contexts that might have been left over from failed tests
+        ContextsPage contextsPage = new ContextsPage(driver);
+        ContextsRowComponent leftoverContext = contextsPage.getTableRowByTitle(contextName);
+        if (leftoverContext != null) {
+            contextsPage.deleteTableRow(leftoverContext).clickOkButton();
+        }
     }
 }
 
