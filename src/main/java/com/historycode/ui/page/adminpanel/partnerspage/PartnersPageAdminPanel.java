18db54d002d67b42c0ebaad6d9b741b4f4670f9d
Ivan <ivanromanuk2014@gmail.com>
2025-01-15 14:19:50 +0200

    Changed the code according to the tips from codeRabbitai

--- src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageAdminPanel.java
+++ src/main/java/com/historycode/ui/page/adminpanel/partnerspage/PartnersPageAdminPanel.java
@@ -2,7 +2,6 @@ package com.historycode.ui.page.adminpanel.partnerspage;
 
 import com.historycode.ui.page.adminpanel.BasePageAdminPanel;
 
-import com.historycode.ui.page.adminpanel.jobspage.JobsPageAdminPanel;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -38,7 +37,10 @@ public class PartnersPageAdminPanel extends BasePageAdminPanel {
         return this;
     }
 
-    public PartnersPageAdminPanel  clickPaginationItem(int index) {
+    public PartnersPageAdminPanel clickPaginationItem(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Pagination index cannot be negative");
+            }
         partnersPageGridComponent.clickPaginationItem(index);
         return this;
     }
