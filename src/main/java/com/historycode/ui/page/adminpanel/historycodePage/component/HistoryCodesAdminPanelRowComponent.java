a6f1945781b43d0ca7fdeb117f600e9b191eb180
Liubomyr Halamaha <galamagal86@gmail.com>
2025-01-16 15:51:11 +0200

    fix

--- src/main/java/com/historycode/ui/page/adminpanel/historycodePage/component/HistoryCodesAdminPanelRowComponent.java
+++ src/main/java/com/historycode/ui/page/adminpanel/historycodePage/component/HistoryCodesAdminPanelRowComponent.java
@@ -7,36 +7,29 @@ import com.historycode.ui.page.adminpanel.historycodePage.HistoryCodesAdminPanel
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 public class HistoryCodesAdminPanelRowComponent extends BaseComponent {
 
     @Getter
+    protected final DropdownComponent dropDown;
+    @Getter
     @FindBy(xpath = "./td[@class='ant-table-cell'][1]")
     protected WebElement name;
-
     @Getter
     @FindBy(xpath = "./td[@class='ant-table-cell'][2]")
     protected WebElement id;
-
-    @Getter
-    protected final DropdownComponent dropDown;
-    @FindBy(xpath = "./td[@class='ant-table-cell'][3]//button[@class='ant-btn css-k7429z ant-btn-default ant-dropdown-trigger']")
-    private WebElement dropDownNode;
-
     @Getter
     @FindBy(xpath = "./td[@class='ant-table-cell'][4]")
     protected WebElement Data;
-
     @FindBy(xpath = ".//td[@class='ant-table-cell'][5]//span[contains(@class, 'anticon-edit')]//*[name()='svg']")
     protected WebElement editPageButton;
-
     @FindBy(xpath = "./td[@class='ant-table-cell'][5]//span[contains(@class, 'anticon-delete')]//*[name()='svg']")
     protected WebElement deleteButton;
-
     @FindBy(xpath = "./td[@class='ant-table-cell'][5]//span[contains(@class, 'anticon-bar-chart')]//*[name()='svg']")
     protected WebElement statisticsPageButton;
+    @FindBy(xpath = "./td[@class='ant-table-cell'][3]//button[@class='ant-btn css-k7429z ant-btn-default ant-dropdown-trigger']")
+    private WebElement dropDownNode;
 
     public HistoryCodesAdminPanelRowComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
@@ -48,18 +41,19 @@ public class HistoryCodesAdminPanelRowComponent extends BaseComponent {
     }
 
     public StreetcodeEditPage clickEdit() {
-        Actions actions = new Actions(driver).moveToElement(editPageButton);
+
+        actions.moveToElement(editPageButton);
         editPageButton.click();
         return new StreetcodeEditPage(driver);
     }
 
     public void clickDelete() {
-        Actions actions = new Actions(driver).moveToElement(deleteButton);
+        actions.moveToElement(deleteButton);
         deleteButton.click();
     }
 
     public HistoryCodesAdminPanelStatisticsPage clickStatistics() {
-        Actions actions = new Actions(driver).moveToElement(statisticsPageButton);
+        actions.moveToElement(statisticsPageButton);
         statisticsPageButton.click();
         return new HistoryCodesAdminPanelStatisticsPage(driver);
     }
