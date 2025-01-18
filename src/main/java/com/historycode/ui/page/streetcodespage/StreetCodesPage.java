f609559bb92ba3d46c292c1756904579371dec2d
yevhen.katruk <fdblackwood@gmail.com>
2025-01-19 00:12:23 +0200

    Fix + refactor

--- src/main/java/com/historycode/ui/page/streetcodespage/StreetCodesPage.java
+++ src/main/java/com/historycode/ui/page/streetcodespage/StreetCodesPage.java
@@ -42,7 +42,7 @@ public class StreetCodesPage extends BasePage {
         streetCodesCatalogComponent.getItemComponents().get(index).getNameNode().click();
         return new StreetCodePage(driver);
     }
-    public void ScrollDownStreetCodes() {
-        waitForElementThenScrollUntilLoaderDisappears(getContainerRootNode());
+    public void scrollDownStreetCodes() {
+        waitForElementThenScrollUntilAllContentLoaded(getContainerRootNode());
     }
 }
