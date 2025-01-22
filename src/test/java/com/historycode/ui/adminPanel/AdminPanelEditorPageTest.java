a9b6e5071c38aa18e2be7ac45ca57c082750ec2a
Basil Jaworski <vasyok9723@gmail.com>
2025-01-22 17:14:26 +0200

    Update: Add first page to editor test cases

--- src/test/java/com/historycode/ui/adminPanel/AdminPanelEditorPageTest.java
+++ src/test/java/com/historycode/ui/adminPanel/AdminPanelEditorPageTest.java
@@ -3,36 +3,32 @@ package com.historycode.ui.adminPanel;
 import com.historycode.ui.page.adminpanel.editorpage.CategoriesPage;
 import com.historycode.ui.page.adminpanel.editorpage.ContextsPage;
 import com.historycode.ui.page.adminpanel.editorpage.TagsPage;
+import com.historycode.ui.page.adminpanel.historycodePage.HistoryCodesAdminPanelPage;
 import com.historycode.ui.testrunners.TestRunnerWithAdmin;
+import com.historycode.ui.testrunners.TestRunnerWithAdminEditor;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Issue;
 import jdk.jfr.Description;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class AdminPanelEditorPageTest extends TestRunnerWithAdmin {
-
-    //TODO Add BeforeClass with login to the admin panel
-    //TODO Add AfterMethod to move back to the main admin panel page
+public class AdminPanelEditorPageTest extends TestRunnerWithAdminEditor {
 
     @Test
     @Issue("110")
     @Epic("(Epic#5) Admin/Other pages")
     @Description("Verify that a new context can be created in the admin panel editor")
     public void verifyOpenAddContextModal() {
-        driver.get(testValueProvider.getBaseUIUrl() + "/admin-panel/editor");
 
-        CategoriesPage categoriesPage = new CategoriesPage(driver);//TODO Change add Click Editor step
-
-        boolean actual = categoriesPage
+        boolean actual = new CategoriesPage(driver)
                 .moveToContexts()
                 .clickAddContext()
                 .isExist();
         Assert.assertTrue(actual);
+
     }
 
     @Test
@@ -40,21 +36,17 @@ public class AdminPanelEditorPageTest extends TestRunnerWithAdmin {
     @Epic("(Epic#5) Admin/Other pages")
     @Description("Verify that context list is displayed")
     public void verifyContextGridIsCorrectDisplayed() {
-        driver.get(testValueProvider.getBaseUIUrl() + "/admin-panel/editor");
 
-        CategoriesPage categoriesPage = new CategoriesPage(driver);//TODO Change add Click Editor step
-
-        ContextsPage contextsPage = categoriesPage.moveToContexts();
-
+        ContextsPage contextsPage = new CategoriesPage(driver)
+                .moveToContexts();
         List<String> expectedGridHeaders = Arrays.asList("Назва", "Дії");
         List<String> actualGridHeaders = contextsPage.getTableHeadersString();
-
+        boolean actual = contextsPage.isGridDisplayed();
         Assert.assertEquals(expectedGridHeaders, actualGridHeaders,
                 "Current headers and expected are not same.");
-
-        boolean actual = contextsPage.isGridDisplayed();
         Assert.assertTrue(actual,
                 "Current rows are not displayed or are displayed incorrectly.");
+
     }
 
     @Test
@@ -62,20 +54,16 @@ public class AdminPanelEditorPageTest extends TestRunnerWithAdmin {
     @Epic("(Epic#5) Admin/Other pages")
     @Description("Verify that tag list is displayed")
     public void verifyTagGridIsCorrectDisplayed() {
-        driver.get(testValueProvider.getBaseUIUrl() + "/admin-panel/editor");
 
-        CategoriesPage categoriesPage = new CategoriesPage(driver);//TODO Change add Click Editor step
-
-        TagsPage tagsPage = categoriesPage.moveToTags();
-
+        TagsPage tagsPage = new CategoriesPage(driver)
+                .moveToTags();
         List<String> expectedGridHeaders = Arrays.asList("Назва", "Дії");
         List<String> actualGridHeaders = tagsPage.getTableHeadersString();
-
+        boolean actual = tagsPage.isGridDisplayed();
         Assert.assertEquals(expectedGridHeaders, actualGridHeaders,
                 "Current headers and expected are not same.");
-
-        boolean actual = tagsPage.isGridDisplayed();
         Assert.assertTrue(actual,
                 "Current rows are not displayed or are displayed incorrectly.");
+
     }
 }
