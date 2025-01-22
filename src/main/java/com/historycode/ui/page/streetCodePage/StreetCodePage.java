5ce6435be3d80a2c9d334a00971139ac19251103
Vova K. <101831233+Vovanchek@users.noreply.github.com>
2025-01-22 10:59:09 +0200

    Added getter and new method

--- src/main/java/com/historycode/ui/page/streetCodePage/StreetCodePage.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/StreetCodePage.java
@@ -3,6 +3,7 @@ package com.historycode.ui.page.streetCodePage;
 import com.historycode.ui.elements.BreadcrumbsElement;
 import com.historycode.ui.elements.ScrollTopButtonElement;
 import com.historycode.ui.page.BasePage;
+import lombok.Getter;
 import com.historycode.ui.page.streetCodePage.components.*;
 import com.historycode.ui.page.streetCodePage.elememts.QuickDonateButtonElement;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class StreetCodePage extends BasePage {
+    @Getter
     @FindBy(xpath = "//div[@class='card']")
     private WebElement mainCardNode;
 
@@ -53,6 +55,7 @@ public class StreetCodePage extends BasePage {
     private MainCardComponent mainCard;
     private StreetCodeTextBlockComponent textBlock;
     private InterestingFactsComponent facts;
+    private InterestingFactsCardComponent factsCard;
     private ChronologyComponent timeline;
     private RelatedPersonasComponent relatedFigures;
     private SourcesComponent sources;
@@ -115,6 +118,12 @@ public class StreetCodePage extends BasePage {
             facts = new InterestingFactsComponent(driver, factsNode);
         }
         return facts;
+    }
+    public InterestingFactsCardComponent getFactsCard() {
+        if (factsCard == null) {
+            factsCard = new InterestingFactsCardComponent(driver, factsNode);
+        }
+        return factsCard;
     }
 
     public ChronologyComponent getTimeline() {
