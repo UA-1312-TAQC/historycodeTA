ad2210b5b24cf9ad2eb8fb7362f6cd6845ae758d
Daniil Rusanov <daniil12099@gmail.com>
2025-01-24 00:01:39 +0200

    remove sleep

--- src/main/java/com/historycode/ui/page/BasePage.java
+++ src/main/java/com/historycode/ui/page/BasePage.java
@@ -43,12 +43,11 @@ public abstract class BasePage extends Base {
     }
 
     public boolean isBurgerMenuVisible() {
-        sleep(5000);
         return burgerMenu.isDisplayed();
     }
 
     public BurgerMenuComponent openBurgerMenu() {
-        sleep(5000);
+        waitUntilElementClickable(burgerMenu);
         burgerMenu.click();
         return this.burgerMenuComponent;
     }
