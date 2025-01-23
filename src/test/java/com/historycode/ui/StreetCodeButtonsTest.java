067059d0189c15009ae7c6ac705a29fbda631e5b
Daniil Rusanov <daniil12099@gmail.com>
2025-01-23 23:08:05 +0200

    test skeleton

---
+++ src/test/java/com/historycode/ui/StreetCodeButtonsTest.java
@@ -0,0 +1,27 @@
+package com.historycode.ui;
+
+import com.historycode.ui.page.homePage.HomePage;
+import com.historycode.ui.page.streetcodecatalogpage.StreetCodeCatalogPage;
+import com.historycode.ui.testrunners.BaseTestRunner;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Issue;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StreetCodeButtonsTest extends BaseTestRunner {
+
+    @BeforeMethod
+    public void goToStreetCode() {
+        new HomePage(driver).openBurgerMenu().clickMenuItem("History-коди");
+        new StreetCodeCatalogPage(driver).clickCatalogItemByName(0);
+    }
+
+    @Test
+    @Issue("54")
+    @Epic("StreetCode page")
+    @Description("Page UP and donate buttons")
+    public void testStreetCodeButtons() {
+        // Test code here
+    }
+}
