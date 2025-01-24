e5e8f54877574a4f7ba7eedddc439a1e9402090d
Daniil Rusanov <daniil12099@gmail.com>
2025-01-24 12:24:00 +0200

    add waiter

--- src/main/java/com/historycode/ui/page/streetcodecatalogpage/StreetCodeCatalogPage.java
+++ src/main/java/com/historycode/ui/page/streetcodecatalogpage/StreetCodeCatalogPage.java
@@ -39,6 +39,7 @@ public class StreetCodeCatalogPage extends BasePage {
     }
 
     public StreetCodePage clickCatalogItemByIndex(int index) {
+        waitUntilElementVisible(streetCodesCatalogComponent.getItemComponents().get(index).getNameNode());
         streetCodesCatalogComponent.getItemComponents().get(index).getNameNode().click();
         return new StreetCodePage(driver);
     }
