dffd569b315c5c93629607fe6fca4ace43b1a390
Daniil Rusanov <daniil12099@gmail.com>
2025-01-25 21:35:37 +0200

    some changes

--- src/test/java/com/historycode/ui/StreetCodeButtonsTest.java
+++ src/test/java/com/historycode/ui/StreetCodeButtonsTest.java
@@ -2,15 +2,20 @@ package com.historycode.ui;
 
 import com.historycode.ui.page.homePage.HomePage;
 import com.historycode.ui.page.streetCodePage.StreetCodePage;
+import com.historycode.ui.page.streetCodePage.modals.DonateModal;
 import com.historycode.ui.testrunners.BaseTestRunner;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Issue;
+import io.qameta.allure.Step;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class StreetCodeButtonsTest extends BaseTestRunner {
+
+    SoftAssert softAssert;
+    StreetCodePage streetCodePage;
 
     @BeforeMethod
     public void goToStreetCode() {
@@ -25,13 +30,51 @@ public class StreetCodeButtonsTest extends BaseTestRunner {
     @Epic("StreetCode page")
     @Description("Page UP and donate buttons")
     public void testStreetCodeButtons() {
-        SoftAssert softAssert = new SoftAssert();
-        StreetCodePage streetCodePage = new StreetCodePage(driver);
+        softAssert = new SoftAssert();
+        streetCodePage = new StreetCodePage(driver);
 
-        streetCodePage.getVerticalProgress().clickSection(2);
+        pageUpButtonTest();
+        donateButtonTest();
 
-        softAssert.assertTrue(streetCodePage.getScrollTopButton().isButtonDisplayed(), "Scroll top button is not visible");
+        // verify questionnaire does not appear at the bottom of the page
+
+        //refresh page
+        //verify questionnaire appears at the bottom of the page
+
+        //verify questionnaire does not appear at the second time at the bottom of the page
 
         softAssert.assertAll();
+    }
+
+    @Step("Page UP button test")
+    private void pageUpButtonTest() {
+        streetCodePage.getVerticalProgress().clickSection(2);
+        softAssert.assertTrue(streetCodePage.getScrollTopButton().isButtonDisplayed(), "Scroll top button is not visible");
+
+        streetCodePage.getScrollTopButton().clickScrollTop();
+        softAssert.assertTrue(streetCodePage.getMainCard().isNameVisible(), "Main card is not visible");
+    }
+
+    @Step("Donate button test")
+    private void donateButtonTest() {
+        softAssert.assertTrue(streetCodePage.getQuickDonateButton().isDonateButtonDisplayed(), "Donate button is not visible");
+
+        DonateModal donateModal = streetCodePage.getQuickDonateButton().clickDonateButton();
+        softAssert.assertTrue(donateModal.isDonateButtonDisplayed(), "Donate popup is not visible");
+
+        // Підтримати unClicable
+        // confirm agreement about personal data
+
+        // Підтримати unClicable
+        // Click on "500 грн"
+
+        // Підтримати Clicable
+
+        donateModal.close();
+    }
+
+    @Step("Questionnaire test")
+    public void goToQuestionnaire() {
+        streetCodePage.scrollToEndOfPage();
     }
 }
