16f3cfd71ca8b26485ed566c1b57df8caf1cfb7f
Daniil Rusanov <daniil12099@gmail.com>
2025-01-25 21:21:43 +0200

    change method clickSection and xpaths

--- src/main/java/com/historycode/ui/page/streetCodePage/components/PageNavigationBarComponent.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/components/PageNavigationBarComponent.java
@@ -6,19 +6,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PageNavigationBarComponent extends BaseComponent {
-    @FindBy(xpath = ".//div[@class='progressBarPopupContent']/div/span")
+    @FindBy(xpath = ".//div[@class='progressBarPopupContent']/div[span]")
     private List<WebElement> sectionNumbers;
 
-    @FindBy(xpath = ".//div[@class='progressBarPopupContainer']")
+    @FindBy(xpath = "./div")
     private WebElement hideButton;
+
+    @FindBy(xpath = ".//div[@class='progressBarPopupContent']")
+    private WebElement progressBar;
 
     public PageNavigationBarComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
     }
 
     public void clickSection(int sectionNumber) {
+        if(!isProgressBarVisible()) {
+            toggleProgressBar();
+        }
+
         if (sectionNumber > 0 && sectionNumber <= sectionNumbers.size()) {
             sectionNumbers.get(sectionNumber - 1).click();
         } else {
@@ -31,7 +39,7 @@ public class PageNavigationBarComponent extends BaseComponent {
     }
 
     public boolean isProgressBarVisible() {
-        return sectionNumbers.get(0).isDisplayed();
+        return progressBar.isDisplayed();
     }
 
     public int getSectionsCount() {
