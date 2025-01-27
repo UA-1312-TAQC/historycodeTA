2be9be722f961719555fa9c1e6b6311c074fc22b
nestea4 <panchoshnaanastasia@gmail.com>
2025-01-27 13:17:16 +0300

    added 77 test case - verify streetcode tags

--- src/main/java/com/historycode/ui/page/streetCodePage/modals/KeywordPersonasModal.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/modals/KeywordPersonasModal.java
@@ -2,10 +2,14 @@ package com.historycode.ui.page.streetCodePage.modals;
 
 import com.historycode.ui.component.BaseModal;
 import com.historycode.ui.page.streetCodePage.components.RelatedPersonasCardComponent;
+import com.historycode.ui.page.streetCodePage.components.TagsPersonasCardComponent;
+import io.qameta.allure.Step;
 import lombok.Getter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -13,32 +17,44 @@ import java.util.stream.Collectors;
 
 public class KeywordPersonasModal extends BaseModal {
     @FindBy(xpath = ".//div[@class='tagModalContainer']//button")
-    private List<WebElement> keywords;
+    private List<WebElement> tagsNode;
 
-    @FindBy(xpath = "//div[@class='relatedFiguresByTagsContentContainer']//a[1]")
+    @Getter
+    @FindBy(xpath = ".//div[@class='relatedFiguresByTagsContentContainer']")
+    private WebElement personsCardsContainer;
+
+    @FindBy(xpath = ".//div[@class='relatedFiguresByTagsContentContainer']//a[1]")
     private List<WebElement> personCards;
 
     @Getter
-    private final List<RelatedPersonasCardComponent> persons;
+    private List<RelatedPersonasCardComponent> persons;
 
     public KeywordPersonasModal(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
-        this.persons = personCards.stream()
-                .map(card -> new RelatedPersonasCardComponent(driver, card))
+    }
+
+    public List<TagsPersonasCardComponent> getPersons() {
+        //wait.until(ExpectedConditions.visibilityOf(personsCardsContainer));
+        waitUntilElementVisible(personsCardsContainer);
+        List<WebElement> currentPersonCards = driver.findElements(
+                By.xpath(".//div[@class='relatedFiguresByTagsContentContainer']/a")
+        );
+
+        return currentPersonCards.stream()
+                .map(card -> new TagsPersonasCardComponent(driver, card))
                 .collect(Collectors.toList());
     }
 
-    public void selectKeyword(String keyword) {
-        keywords.stream()
-                .filter(k -> k.getText().equals(keyword))
-                .findFirst()
-                .orElseThrow(() -> new NoSuchElementException("Keyword not found: " + keyword))
-                .click();
+    @Step("Get selected tag in modal")
+    public String getSelectedTag() {
+        try {
+            WebElement selectedTag = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//div[contains(@class, 'slick-active')]//button[contains(@class, 'tagModalItem')]")
+            ));
+            return selectedTag.getText();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
-    public List<String> getAvailableKeywords() {
-        return keywords.stream()
-                .map(WebElement::getText)
-                .collect(Collectors.toList());
-    }
 }
