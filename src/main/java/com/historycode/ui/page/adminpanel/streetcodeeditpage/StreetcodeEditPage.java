7a1351c319c5aa85c440470df8f5e97aa797de31
Vova K. <101831233+Vovanchek@users.noreply.github.com>
2025-01-29 13:42:56 +0200

    Updated StreetcodeEditPage.java

--- src/main/java/com/historycode/ui/page/adminpanel/streetcodeeditpage/StreetcodeEditPage.java
+++ src/main/java/com/historycode/ui/page/adminpanel/streetcodeeditpage/StreetcodeEditPage.java
@@ -138,6 +138,11 @@ public class StreetcodeEditPage extends BasePageAdminPanel {
     @FindBy(xpath = "//button[@name='Опублікувати']")
     private WebElement publishButton;
 
+    @FindBy(xpath = "(//div[@class='ant-select-selector'])[5]")
+    private WebElement partnersRootElement;
+
+    @FindBy(css = "div.ant-modal-content")
+    private WebElement partnersModalRoot;
 
     public StreetcodeEditPage(WebDriver driver) {
         super(driver);
@@ -179,14 +184,14 @@ public class StreetcodeEditPage extends BasePageAdminPanel {
     }
 
     public DropdownBase getParntersDropdown() {
-        WebElement partnersRootElement = driver.findElement(By.xpath("(//div[@class='ant-select-selector'])[5]"));
+        waitForElementThenScrollUntilAllContentLoaded(partnersRootElement);
         return new DropdownBase(driver, partnersRootElement);
     }
 
     public PartnerModalComponent clickAddPartnerButton() {
-        waitForElementThenScrollUntilAllContentLoaded(addPartnersButton);
+        waitForElementThenScrollUntilAllContentLoaded(partnersRootElement);
         addPartnersButton.click();
-        return new PartnerModalComponent(driver);
+        return new PartnerModalComponent(driver, partnersRootElement);
     }
 
     /**
