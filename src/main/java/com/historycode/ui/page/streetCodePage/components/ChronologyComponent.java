18baaaaf22d5541fbf288092e46e662524fa0ec7
Krystyna Havrysh <gauzerdj@gmail.com>
2025-01-29 16:58:34 +0200

    Change constructor and methods

--- src/main/java/com/historycode/ui/page/streetCodePage/components/ChronologyComponent.java
+++ src/main/java/com/historycode/ui/page/streetCodePage/components/ChronologyComponent.java
@@ -2,23 +2,17 @@ package com.historycode.ui.page.streetCodePage.components;
 
 import com.historycode.ui.component.BaseComponent;
 import lombok.Getter;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.ArrayList;
 import java.util.List;
-
 
 public class ChronologyComponent extends BaseComponent {
 
-    @Getter
-    private final ChronologyYearsBarComponent yearsBar;
+    private ChronologyYearsBarComponent yearsBar;
 
-    @Getter
-    private final ChronologyFilmCardComponent filmCardComponent;
+    private ChronologyFilmCardComponent filmCardComponent;
 
     @FindBy(xpath = ".//div[@id='timeline']//h1")
     private WebElement title;
@@ -28,9 +22,8 @@ public class ChronologyComponent extends BaseComponent {
 
     @Getter
     @FindBy(xpath = ".//div[contains(@class, 'timeSpanContainer')]")
-    private WebElement years;
+    private WebElement year;
 
-    @Getter
     @FindBy(xpath = ".//div[contains(@class, 'timelineYearTicksContainer')]")
     private WebElement greyBox;
 
@@ -39,25 +32,14 @@ public class ChronologyComponent extends BaseComponent {
 
     @Getter
     @FindBy(xpath = ".//div[@class=\"timelineItem\"]/../../..")
-    private List<WebElement> filmCardNode;
+    private List<WebElement> filmCardNodes;
 
-    private  List<ChronologyFilmCardComponent> filmCardComponents;
-
+    private List<ChronologyFilmCardComponent> filmCardComponents;
 
     public ChronologyComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
-        this.yearsBar = new ChronologyYearsBarComponent(driver);
-        this.filmCardComponent = new ChronologyFilmCardComponent(driver);
     }
-    public  List<ChronologyFilmCardComponent> getFilmCard(){
-        if (filmCardComponents == null){
-            filmCardComponents = new ArrayList<>();
-            for(WebElement element: filmCardNode){
-                filmCardComponents.add(new ChronologyFilmCardComponent(driver, element));
-            }
-        }
-        return filmCardComponents;
-    }
+
     public String getTitle() {
         return title.getText();
     }
@@ -74,16 +56,76 @@ public class ChronologyComponent extends BaseComponent {
         return redTimeline;
     }
 
+    public ChronologyYearsBarComponent getYearsBar() {
+        scrollToElement(year);
+        if (yearsBar == null) {
+            WebElement yearsBarRoot = rootElement.findElement(By.xpath(".//div[contains(@class, 'yearsBarContainer')]"));
+            yearsBar = new ChronologyYearsBarComponent(driver, yearsBarRoot);
+        }
+        return yearsBar;
+    }
+
     public WebElement getGreyBox() {
         scrollToElement(greyBox);
         wait.until(ExpectedConditions.visibilityOf(greyBox));
         return greyBox;
     }
 
-    public WebElement getFilmCardContainer() {
+    public ChronologyFilmCardComponent getFilmCardComponent() {
         scrollToElement(filmCardContainer);
-        wait.until(ExpectedConditions.visibilityOf(filmCardContainer));
-        return filmCardContainer;
+        if (filmCardComponent == null) {
+            WebElement filmCardRoot = rootElement.findElement(By.xpath(".//div[contains(@class, 'timelineContentContainer')]"));
+            filmCardComponent = new ChronologyFilmCardComponent(driver, filmCardRoot);
+        }
+        return filmCardComponent;
     }
 
+    public ChronologyFilmCardComponent getFilmCardByIndex(int index) {
+        List<WebElement> filmCardElements = rootElement.findElements(By.xpath(".//div[@class='timelineItem']"));
+
+        if (index < 0 || index >= filmCardElements.size()) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        WebElement filmCardElement = filmCardElements.get(index);
+        scrollToElement(filmCardElement);
+        wait.until(ExpectedConditions.visibilityOf(filmCardElement));
+
+        return new ChronologyFilmCardComponent(driver, filmCardElement);
+    }
+
+    public void clickFilmCardByIndex(int index) {
+        ChronologyFilmCardComponent filmCard = getFilmCardByIndex(index);
+
+        if (filmCard != null) {
+            filmCard.clickFilmCard();
+            System.out.println("Successfully clicked on film card at index: " + index);
+        } else {
+            throw new NoSuchElementException("Film card at index " + index + " not found.");
+        }
+    }
+
+    public void clickYearBoxByIndex(int index) {
+        List<WebElement> yearBoxes = rootElement.findElements(By.xpath(".//div[contains(@class, 'timelineYearTick')]"));
+        if (index < 0 || index >= yearBoxes.size()) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        WebElement yearBox = yearBoxes.get(index);
+        scrollToElement(yearBox);
+        wait.until(ExpectedConditions.elementToBeClickable(yearBox));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", yearBox);
+    }
+
+    public void getYearNodeByIndex(int index) {
+        List<WebElement> yearNodes = rootElement.findElements(By.xpath(".//div[contains(@class, 'timeSpanContainer')]//span"));
+        if (index < 0 || index >= yearNodes.size()) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        WebElement yearNode = yearNodes.get(index);
+        scrollToElement(yearNode);
+        wait.until(ExpectedConditions.visibilityOf(yearNode));
+        yearNode.getText();
+    }
 }
