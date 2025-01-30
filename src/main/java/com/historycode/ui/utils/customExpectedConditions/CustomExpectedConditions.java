91810235176ac453f22df00b8c3ff815e6f1d5aa
Viktor Kravchenko <viktorkrav03@gmail.com>
2025-01-30 16:12:47 +0200

    Fixed custom expectd condition

--- src/main/java/com/historycode/ui/utils/customExpectedConditions/CustomExpectedConditions.java
+++ src/main/java/com/historycode/ui/utils/customExpectedConditions/CustomExpectedConditions.java
@@ -2,6 +2,7 @@ package com.historycode.ui.utils.customExpectedConditions;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.StaleElementReferenceException;
@@ -13,23 +14,29 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 @Slf4j
-public class CustomExpectedConditions {
+public class CustomExpectedConditions implements ExpectedCondition<Boolean> {
 
-    public static ExpectedCondition<Boolean> stalenessOfElementLocatedBy(By locator) {
-        return new ExpectedCondition<Boolean>(){
-            @Override
-            public Boolean apply(WebDriver driver) {
-                log.debug("Applying custom expected condition");
-                try{
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-                    driver.findElement(locator);
-                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-                }catch(StaleElementReferenceException | NoSuchElementException ex){
-                    log.debug("Expected condition is true: " + ex.getClass().toString() + " is thrown");
-                    return true;
-                }
-                return false;
-            }
-        };
+    String locator;
+
+    public CustomExpectedConditions(String locator) {
+        this.locator = locator;
+    }
+
+    @Override
+    public Boolean apply(WebDriver driver) {
+        log.debug("Applying custom expected condition");
+        Duration timeout = driver.manage().timeouts().getImplicitWaitTimeout();
+        log.debug("Implicit wait " + timeout);
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+            driver.findElement(By.cssSelector(this.locator));
+        } catch (StaleElementReferenceException | NoSuchElementException ex) {
+            log.debug("Expected condition is true: " + ex.getClass().toString() + " is thrown");
+            return true;
+        }finally {
+            driver.manage().timeouts().implicitlyWait(timeout);
+        }
+        return false;
     }
 }
+
