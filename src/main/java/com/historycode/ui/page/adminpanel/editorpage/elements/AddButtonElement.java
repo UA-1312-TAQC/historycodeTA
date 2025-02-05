e4c4b234bfdee7f6f4fde699857a57323c5cd999
Basil Jaworski <vasyok9723@gmail.com>
2025-02-05 13:07:28 +0200

    Update: AdminPanelEditor elements added @Getter annotations instead getSomething

--- src/main/java/com/historycode/ui/page/adminpanel/editorpage/elements/AddButtonElement.java
+++ src/main/java/com/historycode/ui/page/adminpanel/editorpage/elements/AddButtonElement.java
@@ -2,14 +2,17 @@ package com.historycode.ui.page.adminpanel.editorpage.elements;
 
 import com.historycode.ui.component.BaseComponent;
 import io.qameta.allure.Step;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class AddButtonElement extends BaseComponent {
 
+    @Getter
     @FindBy(xpath = "./button[contains(@class, 'add-button')]")
     private WebElement button;
+    @Getter
     @FindBy(xpath = "./button[contains(@class, 'add-button')]//span")
     private WebElement label;
 
