379586c3b6ebcc95cd1917b24009886883762937
Basil Jaworski <vasyok9723@gmail.com>
2025-02-06 12:44:45 +0200

    Begin: Issue #109

--- src/test/java/com/historycode/ui/adminPanel/EditorPage/EditorPositionsTests.java
+++ src/test/java/com/historycode/ui/adminPanel/EditorPage/EditorPositionsTests.java
@@ -6,12 +6,14 @@ import com.historycode.ui.testrunners.TestRunnerWithAdminEditor;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Issue;
 import jdk.jfr.Description;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EditorPositionsTests extends TestRunnerWithAdminEditor {
 
-    private final String TEST_POSITION = "test_Position23";
-    private final String TEST_POSITION_NEW = "test_Position32";
+    private final String TEST_POSITION = "testPosition23";
+    private final String TEST_POSITION_NEW = "testPosition32";
+    private final String TEST_POSITION_TOO_LONG = "testPositionAddingWithATooLongNameMoreThan50Symbols";
 
     @Test
     @Issue("100")
@@ -28,6 +30,26 @@ public class EditorPositionsTests extends TestRunnerWithAdminEditor {
 
         //ToDo Add Search in grids to the get table row
         //ToDo Add Assert is new position where updated
+    }
+
+    @Test
+    @Issue("109")
+    @Epic("(Epic#5) Admin/Other pages")
+    @Description("Verify that the system blocks the admin's attempt to add more than 50 symbols in the \"Назва\" field")
+    public void verifyAdminCantAddLongPositionName() {
+
+        String actualMessage = new CategoriesPage(driver)
+                .moveToPositions()
+                .clickAddPosition()
+                .enterPosition(TEST_POSITION_TOO_LONG)
+                .getError()
+                .getText();
+
+        String expectedMessage = "Filling out no more than 50 symbols is available.";
+        Assert.assertEquals(actualMessage, expectedMessage);
+
+        //ToDo Update modals error messages
+        //ToDo Check whether expected message is the same on web page
     }
 
     //ToDo Add Before Class method to add new position than move back to Base Admin Page and move to the editor
