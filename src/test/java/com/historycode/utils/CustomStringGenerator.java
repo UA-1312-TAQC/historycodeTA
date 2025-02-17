1c0364beb00e4a7110e1c44a64c17809debced2a
Viktor Kravchenko <viktorkrav03@gmail.com>
2025-02-17 14:07:28 +0200

    cleanup

--- src/test/java/com/historycode/utils/CustomStringGenerator.java
+++ src/test/java/com/historycode/utils/CustomStringGenerator.java
@@ -2,10 +2,6 @@ package com.historycode.utils;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
-/**
- *
- *
- */
 public class CustomStringGenerator {
     public static String generateUserLastFirstName(int nameLength, int surnameLength){
         return RandomStringUtils.randomAlphabetic(nameLength) + " " + RandomStringUtils.randomAlphabetic(surnameLength);
