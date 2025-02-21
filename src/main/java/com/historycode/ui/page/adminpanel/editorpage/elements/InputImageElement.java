6db0be4396e6307277ddff281fc2e88a392eaecd
Basil Jaworski <vasyok9723@gmail.com>
2025-02-21 20:36:08 +0200

    Add: Input element for image on Editor page

---
+++ src/main/java/com/historycode/ui/page/adminpanel/editorpage/elements/InputImageElement.java
@@ -0,0 +1,42 @@
+package com.historycode.ui.page.adminpanel.editorpage.elements;
+
+import com.historycode.ui.component.BaseComponent;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Gertter
+public class InputImageElement extends BaseComponent {
+
+    @FindBy(xpath = ".//label[@for='image']")
+    private WebElement label;
+
+    @FindBy(xpath = ".//span[@class='ant-upload']")
+    private WebElement uploadPlace;
+
+    @FindBy(xpath = ".//div[@class='ant-upload-list-item-container']")
+    private WebElement uploadedPlace;
+
+    @FindBy(xpath = ".//span[@role='img' and @aria-label='eye']")
+    private WebElement previewButton;
+
+    @FindBy(xpath = ".//button[contains(@class, 'ant-btn-icon-only') and @title='Remove file']")
+    private WebElement deleteButton;
+
+    public InputImageElement(WebDriver driver, WebElement rootElement) {
+        super(driver, rootElement);
+    }
+
+    public void clickPreviewButton() {
+        waitUntilElementClickable(previewButton);
+        scrollToElementJs(previewButton);
+        previewButton.click();
+    }
+
+    public void clickDeleteButton() {
+        waitUntilElementClickable(deleteButton);
+        scrollToElementJs(deleteButton);
+        deleteButton.click();
+    }
+
+}
