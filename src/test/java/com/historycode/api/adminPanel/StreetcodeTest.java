fbe2f458fb91a4ecab41398925302535cc1af90f
Viktor Kravchenko <viktorkrav03@gmail.com>
2025-02-28 10:18:54 +0200

    Test case fixed but not tested

--- src/test/java/com/historycode/api/adminPanel/StreetcodeTest.java
+++ src/test/java/com/historycode/api/adminPanel/StreetcodeTest.java
@@ -5,6 +5,7 @@ import com.historycode.api.models.adminPanel.streetcode.ImageDetails;
 import com.historycode.api.models.adminPanel.streetcode.StreetcodeRequestBody;
 import com.historycode.api.models.adminPanel.streetcode.Subtitle;
 import com.historycode.api.testRunners.ApiTestRunner;
+import com.historycode.utils.ImageCreator;
 import io.qameta.allure.Issue;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +35,7 @@ public class StreetcodeTest extends ApiTestRunner {
     public void createStreetcodeTest() {
 
         String dateString = "2024-03-31T13:46:48.769Z";
+        int imageId = ImageCreator.createNewImg("src/test/resources/logo.jpe");
         Instant instant = Instant.parse(dateString);
         ImageDetails imageDetails = new ImageDetails()
                 .setId(0)
@@ -50,7 +52,7 @@ public class StreetcodeTest extends ApiTestRunner {
                 .setEventStartOrPersonBirthDate(instant)
                 .setTags(new ArrayList<>())
                 .setTeaser("efasf")
-                .setImagesIds(List.of(5879))
+                .setImagesIds(List.of(imageId))
                 .setImagesDetails(List.of(imageDetails))
                 .setVideos(new ArrayList<>())
                 .setFacts(new ArrayList<>())
@@ -70,7 +72,7 @@ public class StreetcodeTest extends ApiTestRunner {
         Response response = client.createStreetcode(requestBody);
         Assert.assertEquals(response.getStatusCode(), 200);
         response.body().print();
-        streetcodeId = response.jsonPath().getInt("id");
+        streetcodeId = response.getBody().jsonPath().getInt("id");
         Assert.assertNotEquals(streetcodeId, 0);
     }
 
